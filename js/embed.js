a7d2edda23984cfc02e8ece7483f3d96ceef9607
Redy <19371836+insoxin@users.noreply.github.com>
2021-09-15 12:33:02 +0800

    Update embed.js

--- js/embed.js
+++ js/embed.js
@@ -1,3 +1,81 @@
+export default {
+	data() {
+		return {
+			percentage: 0 //进度条，用户在页面上显示进度
+			loading: false
+		}
+	},
+	methods: {
+		//文件大小限制
+		handleBeforeUpload(file, fileList) {
+		  let isLimit = file.size / 1024 / 1024 <= 1000;
+		  if (!isLimit) {
+		    this.$message.warning("上传文件不超过1G");
+		    return false;
+		  }
+		},
+		
+		//进度条显示
+		handleProgeress(percent) {
+		    this.percentage = percent;
+		},
+	
+		//上传视频
+		uploadVideoHandle(event) {
+	      let file = event.file;
+	      let fileType = 3; // 图片1，音频2，视频3，文件4
+	      this.loading = true;
+	      uploadApi.upLoad
+	        .uploader(file, fileType, event)
+	        .then((res) => {
+	          //上传成功的业务逻辑处理
+	          this.loading = false;
+	          this.percentage = 0;
+	        })
+	        .catch((err) => {
+	          this.$message.error(err.message);
+	          this.loading = false;
+	        });
+	    },
+	}
+}
+import request from "@/utils/request";
+const getAliOSSCreds = (params) => {
+  return request({
+    url: "admin/base/getOssToken",
+    method: "get",
+    params
+  });
+};
+
+export const upLoad = {
+  uploader: (item, fileType, option) => {
+    let params = {};
+    params.type = fileType;
+    return getAliOSSCreds(params).then((res) => {
+      const Oss = require("ali-oss");
+      const client = new Oss({
+        region: res.region,
+        secure: true,
+        accessKeyId: res.accessKeyId,
+        accessKeySecret: res.accessKeySecret,
+        stsToken: res.securityToken,
+        bucket: res.bucket,
+        endpoint: res.endpoint
+      });
+      let key = res.dir + "/" + item.name;
+      if (option != null) {
+        return client.multipartUpload(key, item, { //切片上传
+          progress: function (p, checkpoint) {
+            option.onProgress({percent: Math.floor(p * 100)}); // 触发el-upload组件的onProgress方法
+          }
+        })
+      } else {
+        return client.put(key, item);
+      }
+    });
+  }
+};
 function copyurl(node){
 	var clipboard = new ClipboardJS(".copy-btn", {
 		text: function(trigger) {
